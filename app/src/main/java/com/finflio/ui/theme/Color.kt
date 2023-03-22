cdbf148ea1047fdba65cb0ec7e17e6df4a6c316e
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-03-23 00:49:31 +0530

    Add fonts, icons and colors

--- app/src/main/java/com/finflio/ui/theme/Color.kt
+++ app/src/main/java/com/finflio/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val RedGradient = listOf(Color(0xFFE3823C), Color(0xFFE33C3C))
+val GoldGradient = listOf(Color(0xFFE3D23C), Color.Transparent)
+val GreenGradient = listOf(Color(0xFF7FF6A0), Color(0xFF34D360))
+
+val SecondaryText = Color(0xFF9EA5BD)
+
+val Income = Color(0xFF00A86B)
+val Expense = Color(0xFFE33C3C)
--- app/src/main/java/com/finflio/ui/theme/Type.kt
+++ app/src/main/java/com/finflio/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.finflio.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.finflio.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,16 +16,33 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+)
+
+val Inter = FontFamily(
+    listOf(
+        Font(R.font.inter_regular, FontWeight.Normal),
+        Font(R.font.inter_medium, FontWeight.Medium),
+        Font(R.font.inter_semibold, FontWeight.SemiBold),
+        Font(R.font.inter_bold, FontWeight.Bold),
     )
-    */
+)
+
+val DMSans = FontFamily(
+    listOf(
+        Font(R.font.dmsans_bold, FontWeight.Bold),
+        Font(R.font.dmsans_bolditalic, FontWeight.Bold, FontStyle.Italic),
+        Font(R.font.dmsans_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.dmsans_medium, FontWeight.Medium),
+        Font(R.font.dmsans_regular, FontWeight.Normal),
+        Font(R.font.dmsans_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    )
+)
+
+val Poppins = FontFamily(
+    listOf(
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_medium, FontWeight.Medium)
+    )
 )
