b76c3d9ec9b569596723267890e2b85c8b03e527
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-03-23 01:06:43 +0530

    Add system Ui Controller
    Made top app bar transparent and gave bottom navigation bar same color as app's bottom nav

--- app/src/main/java/com/finflio/MainActivity.kt
+++ app/src/main/java/com/finflio/MainActivity.kt
@@ -3,15 +3,26 @@ package com.finflio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.finflio.ui.theme.BottomNav
 import com.finflio.ui.theme.FinflioTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             FinflioTheme {
-                // A surface container using the 'background' color from the theme
-
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    // set transparent color so that our image is visible
+                    // behind the status bar
+                    systemUiController.setStatusBarColor(color = Color.Transparent)
+                    systemUiController.setNavigationBarColor(color = BottomNav)
+                }
             }
         }
     }
--- app/src/main/java/com/finflio/ui/theme/Color.kt
+++ app/src/main/java/com/finflio/ui/theme/Color.kt
@@ -7,6 +7,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val BottomNav = Color(0x1A141326)
 val RedGradient = listOf(Color(0xFFE3823C), Color(0xFFE33C3C))
 val GoldGradient = listOf(Color(0xFFE3D23C), Color.Transparent)
 val GreenGradient = listOf(Color(0xFF7FF6A0), Color(0xFF34D360))
