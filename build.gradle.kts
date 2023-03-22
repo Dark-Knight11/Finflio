f8d1ca0ba2cde429b2abbf81e93a2f62170eac40
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-03-23 01:03:53 +0530

    Add Experimental annotations all over the codebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,22 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
 }
+
+allprojects {
+    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                // Avoid having to stutter experimental annotations all over the codebase
+                "-opt-in=androidx.compose.ui.text.ExperimentalTextApi",
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
+                "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
+                "-opt-in=androidx.compose.material.ExperimentalMaterialApi",
+                "-opt-in=androidx.compose.animation.ExperimentalAnimationApi",
+                "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+                "-opt-in=kotlinx.coroutines.DelicateCoroutinesApi",
+                "-opt-in=com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi",
+            )
+        }
+    }
+}
