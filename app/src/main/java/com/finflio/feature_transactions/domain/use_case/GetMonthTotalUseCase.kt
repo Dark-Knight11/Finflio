1df7b2b54151c54fe5c4a51912e76c0095d622df
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-04-07 17:29:11 +0530

    optimise the code and now only displaying total expense

--- app/src/main/java/com/finflio/feature_transactions/domain/use_case/GetMonthTotalUseCase.kt
+++ app/src/main/java/com/finflio/feature_transactions/domain/use_case/GetMonthTotalUseCase.kt
@@ -1,6 +1,7 @@
 package com.finflio.feature_transactions.domain.use_case
 
 import com.finflio.feature_transactions.domain.repository.TransactionsRepository
+import com.patrykandpatrick.vico.core.extension.sumByFloat
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -13,16 +14,15 @@ class GetMonthTotalUseCase @Inject constructor(
     suspend operator fun invoke(month: String): Flow<Float> {
         return channelFlow {
             repo.getTransactions().collectLatest { list ->
-                var total = 0f
-                list.filter {
-                    it.timestamp.toLocalDate().month == Month.valueOf(month.uppercase())
-                }.forEach {
-                    if (it.type == "Income")
-                        total -= it.amount
-                    else total += it.amount
-                }
-                println("Total: $total")
-                send(total)
+                send(
+                    list.sumByFloat { transaction ->
+                        if (transaction.type == "Expense" &&
+                            transaction.timestamp.toLocalDate().month == Month.valueOf(month.uppercase())
+                        ) {
+                            transaction.amount
+                        } else 0f
+                    }
+                )
             }
         }
     }
