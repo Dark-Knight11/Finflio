2073f8191344574760852f20f01315aa5fc8d225
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-04-27 23:26:50 +0530

    migrate to use parcelize plugin

--- app/src/main/java/com/finflio/core/domain/model/Transaction.kt
+++ app/src/main/java/com/finflio/core/domain/model/Transaction.kt
@@ -1,9 +1,10 @@
 package com.finflio.core.domain.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
 
+@Parcelize
 data class Transaction(
     val transactionId: Int = 0,
     val timestamp: LocalDateTime,
@@ -15,45 +16,4 @@ data class Transaction(
     val attachment: String? = null,
     val from: String? = null,
     val to: String? = null
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readValue(LocalDateTime::class.java.classLoader) as LocalDateTime,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readFloat(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(transactionId)
-        parcel.writeSerializable(timestamp)
-        parcel.writeString(type)
-        parcel.writeString(category)
-        parcel.writeString(paymentMethod)
-        parcel.writeString(description)
-        parcel.writeFloat(amount)
-        parcel.writeString(attachment)
-        parcel.writeString(from)
-        parcel.writeString(to)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Transaction> {
-        override fun createFromParcel(parcel: Parcel): Transaction {
-            return Transaction(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Transaction?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
