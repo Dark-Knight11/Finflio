410868e693d560b6a7fa864b30996c97f52a7069
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-06-21 00:57:35 +0530

    Move Cloudinary Initialization

--- app/src/main/java/com/finflio/FinflioApp.kt
+++ app/src/main/java/com/finflio/FinflioApp.kt
@@ -1,7 +1,18 @@
 package com.finflio
 
 import android.app.Application
+import com.cloudinary.android.MediaManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FinflioApp : Application()
+class FinflioApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val config: MutableMap<String, String> = HashMap()
+        config["cloud_name"] = BuildConfig.CLOUD_NAME
+        config["api_key"] = BuildConfig.CLOUDINARY_API_KEY
+        config["api_secret"] = BuildConfig.CLOUDINARY_API_SECRET
+        MediaManager.init(this, config)
+    }
+}
