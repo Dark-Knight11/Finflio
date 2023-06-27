4cb43fb0198bf859e618fdec7426f9dc6e3982ae
Dark-Knight11 <vedantbhamare11@gmail.com>
2023-06-27 16:47:09 +0530

    fix exception message

--- app/src/main/java/com/finflio/feature_transactions/domain/util/InvalidTransactionException.kt
+++ app/src/main/java/com/finflio/feature_transactions/domain/util/InvalidTransactionException.kt
@@ -1,27 +1,20 @@
 package com.finflio.feature_transactions.domain.util
 
 import com.finflio.feature_transactions.data.models.remote.TransactionPostRequest
+
 class InvalidTransactionException(message: String) : Exception(message)
 
 fun errors(transactionPostRequest: TransactionPostRequest) {
     if (transactionPostRequest.category.isBlank()) {
-        throw InvalidTransactionException(
-            "The category of the transactionPostRequest can't be empty."
-        )
+        throw InvalidTransactionException("The category of the transaction can't be empty.")
     } else if (transactionPostRequest.description.isBlank()) {
-        throw InvalidTransactionException(
-            "The description of the transactionPostRequest can't be empty."
-        )
+        throw InvalidTransactionException("The description of the transaction can't be empty.")
     } else if (transactionPostRequest.paymentMethod.isBlank()) {
-        throw InvalidTransactionException(
-            "The paymentMethod of the transactionPostRequest can't be empty."
-        )
+        throw InvalidTransactionException("The paymentMethod of the transaction can't be empty.")
     } else if (transactionPostRequest.type.isBlank()) {
-        throw InvalidTransactionException("The type of the transactionPostRequest can't be empty.")
+        throw InvalidTransactionException("The type of the transaction can't be empty.")
     } else if (transactionPostRequest.description.isBlank()) {
-        throw InvalidTransactionException(
-            "The description of the transactionPostRequest can't be empty."
-        )
+        throw InvalidTransactionException("The description of the transaction can't be empty.")
     } else if (transactionPostRequest.type == "Income" && transactionPostRequest.to?.isNotBlank() == true) {
         throw InvalidTransactionException("To field is applicable only for Expense.")
     } else if (transactionPostRequest.type == "Expense" && transactionPostRequest.from?.isNotBlank() == true) {
